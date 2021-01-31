7ed5c283d5aa29ebb03efecb40a3bbe49ce39e5b
Ritvik Dayal <ritvikr1605@gmail.com>
2021-01-31 13:43:15 +0530

    tested and fixed permissions

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissions
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, IsAuthenticatedOrReadOnly
 
 class PostUserWritePermission(BasePermission):
     message = 'Editting Posts is restricted to Author Only!'
@@ -13,7 +13,7 @@ class PostUserWritePermission(BasePermission):
         return obj.author == request.user
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
 
