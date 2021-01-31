15b27111638ccaa5e54ee54682eead296cb3a0d5
Ritvik Dayal <ritvikr1605@gmail.com>
2021-01-31 13:45:47 +0530

    beautified code

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -39,12 +39,21 @@ class PostTests(APITestCase):
         client = APIClient()
 
         self.test_category = Category.objects.create(name='django')
+
         self.testuser1 = User.objects.create_user(
             username='test_user1', password='123456789')
         self.testuser2 = User.objects.create_user(
             username='test_user2', password='123456789')
+        
         test_post = Post.objects.create(
-            category_id=1, title='Post Title', excerpt='Post Excerpt', content='Post Content', slug='post-title', author_id=1, status='published')
+            category_id=1,
+            title='Post Title', 
+            excerpt='Post Excerpt', 
+            content='Post Content', 
+            slug='post-title', 
+            author_id=1, 
+            status='published'
+        )
 
         client.login(username=self.testuser1.username,
                      password='123456789')
@@ -59,6 +68,7 @@ class PostTests(APITestCase):
                 "content": "New",
                 "status": "published"
             }, format='json')
+            
         print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
